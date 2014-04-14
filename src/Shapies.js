760e754807996c45fe8b630f5161b33c4c4eed20
Nassar Zeitoune <almightynassar@gmail.com>
2014-04-14 23:25:37 +1000

    Added a whole lot of changes, including the Lightbulb drawing and the Canvas context extensions. Signed-off-by: Nassar Zeitoune <almightynassar@gmail.com>

--- src/Shapies.js
+++ src/Shapies.js
@@ -1,3 +1,180 @@
+/* ===========================================
+ * JavaScript HTML5 Canvas extensions
+ * ===========================================
+ */
+
+if (window.CanvasRenderingContext2D) {
+    /**
+     * Cavas reset
+     */
+    CanvasRenderingContext2D.prototype.reset= function() {
+        this.translate(0,0);
+        this.fillStyle = "white";
+        this.strokeStyle = "white";
+    };
+    /**
+     * Rounded Rectangle
+     */
+    CanvasRenderingContext2D.prototype.roundedRect = function(x, y, width, height, radius, colour, border, lineWidth, alpha) {
+        // if certain values are not set just exit
+        if(!x || !y || !width || !height) { return true; }
+        // Set other values
+        if (!radius) { radius = 5; }
+        if (!alpha) { alpha=1; }
+        // Start drawing the rounded rectangle
+        var x_left = x - (width/2);
+        var x_right = x + (width/2);
+        var y_top = y - (height/2);
+        var y_bot = y + (height/2);
+        this.save();
+        this.reset();
+        this.beginPath();
+        this.moveTo(x_left + radius, y_top);
+        this.lineTo(x_right - radius, y_top);
+        this.quadraticCurveTo(x_right, y_top, x_right, y_top + radius);
+        this.lineTo(x_right, y_bot - radius);
+        this.quadraticCurveTo(x_right, y_bot, x_right - radius, y_bot);
+        this.lineTo(x_left + radius, y_bot);
+        this.quadraticCurveTo(x_left, y_bot, x_left, y_bot - radius);
+        this.lineTo(x_left, y_top + radius);
+        this.quadraticCurveTo(x_left, y_top, x_left + radius, y_top);
+        // Colour it in
+        if (colour) {
+            this.globalAlpha = alpha;
+            this.fillStyle = colour;
+            this.fill();
+        }
+        // Add in optional border
+        if (border) {
+            this.lineWidth = (lineWidth) ? lineWidth : 1;
+            this.strokeStyle = border;
+            this.stroke();
+        }
+        this.closePath();
+        this.restore();
+    };
+    /**
+     * Ellipse
+     */
+    CanvasRenderingContext2D.prototype.ellipse = function(x, y, width, height, colour, border, lineWidth, alpha) {
+        // if certain values are not set just exit
+        if (!x || !y || !width || !height) { return true; }
+        if (!alpha) { alpha=1; }
+        // Calculate some points
+        var xLeft = x - (width / 2);
+        var xRight = x + (width / 2);
+        var yTop = y - (height / 2);
+        var yBot = y + (height / 2);
+        // Draw the ellipse
+        this.save();
+        this.reset();
+        this.beginPath();
+        // NOTE: The moveTo() is needed to start the drawing from the correct spot
+        this.moveTo(x, yTop);
+        // Start drawing two bezier curves to create an ellipse
+        this.bezierCurveTo(xRight, yTop, xRight, yBot, x, yBot);
+        this.bezierCurveTo(xLeft, yBot, xLeft, yTop, x, yTop);
+        // Colour it in
+        if (colour) {
+            this.globalAlpha = alpha;
+            this.fillStyle = colour;
+            this.fill();
+        }
+        // Add in optional border
+        if (border) {
+            this.lineWidth = (lineWidth) ? lineWidth : 1;
+            this.strokeStyle = border;
+            this.stroke();
+        }
+        // Finish the drawing
+        this.closePath();
+        this.restore();
+    };
+    /**
+     * Rounded arcs and circles
+     */
+    CanvasRenderingContext2D.prototype.circularArc = function(x, y, radius, startAng, endAng, colour, border, lineWidth, enclosed, direction, alpha) {
+        // if certain values are not set just exit
+        if (!x || !y || !radius) { return true; }
+        // Set other values
+        if (!alpha) { alpha=1; }
+        if (!startAng) { startAng = 0; }
+        if (!endAng) { endAng = Math.PI; }
+        if (!direction) { direction = false; }
+        // Start drawing
+        this.save();
+        this.reset();
+        this.beginPath();
+        this.arc(x, y, radius, startAng, endAng, direction);
+        this.globalAlpha = alpha;
+        if (enclosed) {
+            this.closePath();
+        }
+        // Colour it in
+        if (colour) {
+            this.fillStyle = colour;
+            this.fill();
+        }
+        // Add in optional border
+        if (border) {
+            this.lineWidth = (lineWidth) ? lineWidth : 1;
+            this.strokeStyle = border;
+            this.stroke();
+        }
+        // Finish the drawing
+        this.closePath();
+        this.restore();
+    };
+    /**
+     * Lightbulb
+     */
+    CanvasRenderingContext2D.prototype.lightbulb = function(x, y, size, colour, border, lineWidth, alpha, baseColour) {
+        // if certain values are not set just exit
+        if (!x || !y || !size) { return true; }
+        // Some standard points for the bulb curve
+        var sizeBy3 = size/3;
+        var sizeBy2 = size/2;
+        var x_right = x+size;
+        var x_left = x-size;
+        var y_top = y-size;
+        var y_bot = y+size;
+        // Set other values
+        if (!alpha) { alpha=1; }
+        this.save();
+        this.reset();
+        this.beginPath();
+        // The bulb
+        this.moveTo(x_right,y);
+        this.bezierCurveTo(x_right,y+sizeBy3,x+sizeBy2,y+sizeBy2,x+sizeBy2,y_bot);
+        this.lineTo(x-sizeBy2,y_bot);
+        this.bezierCurveTo(x-sizeBy2,y+sizeBy2,x_left,y+sizeBy3,x_left,y);
+        this.bezierCurveTo(x_left,y-sizeBy2,x-sizeBy2,y_top,x,y_top);
+        this.bezierCurveTo(x+sizeBy2,y_top,x_right,y-sizeBy2,x_right,y);
+        // Colour it in
+        if (colour) {
+            this.globalAlpha = alpha;
+            this.fillStyle = colour;
+            this.fill();
+        }
+        // Add in optional border
+        if (border) {
+            this.lineWidth = (lineWidth) ? lineWidth : 1;
+            this.strokeStyle = border;
+            this.stroke();
+        }
+        this.closePath();
+        this.restore();
+        if (baseColour) {
+            this.roundedRect(x, y_bot+(sizeBy3/2), size, sizeBy3, 5, baseColour, border, lineWidth, alpha);
+        }
+    };
+}
+
+/* ===========================================
+ * Shapies main code
+ * ===========================================
+ */
+
 /**
  * The Shapies system
  * 
@@ -191,6 +368,10 @@ var Shapies = (function(window) {
                         return (localCanvas.height = newHeight);
                     }
                     return localCanvas.height;
+                },
+                
+                context: function() {
+                    return localCanvas.getContext('2d');
                 }
             };
         };
@@ -240,6 +421,35 @@ var Shapies = (function(window) {
             };
         };
         
+        /**
+         * Draw an arc (either enclosed or open)
+         */
+        function RoundedRectangle() {
+            /**
+             * Some variables
+             */
+            this.colour = 'clear';
+            this.border = 'clear';
+            this.radius = 10;
+            this.point = [0,0];
+            this.length = [5,10];
+            this.lineWidth = 1;
+            
+            /**
+             * Draw a circle onto the canvas
+             */
+            this.draw = function(context) {
+                try {
+                    context.roundedRect(this.point[0],this.point[1], this.length[0], this.length[1], this.radius, this.colour, this.border, this.lineWidth, this.alpha);
+                    return true;
+                } catch(err) {
+                    console.log("ERROR - RoundedRectangle: draw() hit an error");
+                    console.log(err.name);
+                    console.log(err.message);
+                }
+                return false;
+            };
+        };
         
         /**
          * Draw an arc (either enclosed or open)
@@ -251,33 +461,18 @@ var Shapies = (function(window) {
             this.enclosed = false;
             this.colour = 'clear';
             this.border = 'clear';
-            this.angles = [0, Math.PI];
-            this.lengths = [5, 10];
+            this.angles = [0, 2*Math.PI];
+            this.radius = 10;
             this.point = [0,0];
+            this.lineWidth = 1;
+            this.direction = false;
             
             /**
              * Draw a circle onto the canvas
              */
             this.draw = function(context) {
                 try {
-                    context.beginPath();
-                    context.arc(this.point[0], this.point[1], this.radius, this.angles[0], this.angles[1], false);
-                    if (this.enclosed) {
-                        context.closePath();
-                    }
-                    // Colour it in
-                    if (this.colour !== 'clear') {
-                        context.fillStyle = this.colour;
-                        context.fill();
-                    }
-                    // Add in optional border
-                    if (this.border !== 'clear') {
-                        context.lineWidth = 1;
-                        context.strokeStyle = this.border;
-                        context.stroke();
-                    }
-                    // Finish the drawing
-                    context.closePath();
+                    context.circularArc(this.point[0], this.point[1], this.radius, this.angles[0], this.angles[1], this.colour, this.border, this.lineWidth, this.enclosed, this.direction);
                     return true;
                 } catch(err) {
                     console.log("ERROR - ARC: draw() hit an error");
@@ -299,38 +494,49 @@ var Shapies = (function(window) {
             this.border = 'clear';
             this.lengths = [5, 10];
             this.point = [0,0];
+            this.lineWidth = 1;
+            this.alpha = 1;
             
             /**
              * Draw an ellipse onto the canvas
              */
             this.draw = function(context) {
                 try {
-                    context.beginPath();
-                    var xLeft = this.point[0] - (this.lengths[0] / 2);
-                    var xRight = this.point[0] + (this.lengths[0] / 2);
-                    var yTop = this.point[1] - (this.lengths[1] / 2);
-                    var yBot = this.point[1] + (this.lengths[1] / 2);
-                    // NOTE: The moveTo() is needed to start the drawing from the correct spot
-                    context.moveTo(this.point[0], yTop);
-                    // Start drawing two bezier curves to create an ellipse
-                    context.bezierCurveTo(xRight, yTop, xRight, yBot, this.point[0], yBot);
-                    context.bezierCurveTo(xLeft, yBot, xLeft, yTop, this.point[0], yTop);
-                    // Colour it in
-                    if (this.colour !== 'clear') {
-                        context.fillStyle = this.colour;
-                        context.fill();
-                    }
-                    // Add in optional border
-                    if (this.border !== 'clear') {
-                        context.lineWidth = 1;
-                        context.strokeStyle = this.border;
-                        context.stroke();
-                    }
-                    // Finish the drawing
-                    context.closePath();
+                    context.ellipse(this.point[0], this.point[1], this.lengths[0], this.lengths[1], this.colour, this.border, this.lineWidth, this.alpha);
                     return true;
                 } catch(err) {
                     console.log("ERROR - ELLIPSE: draw() hit an error");
+                    console.log(err.name);
+                    console.log(err.message);
+                }
+                return false;
+            };
+        };
+        
+        /**
+         * Draw an Lightbulb
+         */
+        function Lightbulb() {
+            /**
+             * Some variables
+             */
+            this.colour = 'clear';
+            this.border = 'clear';
+            this.base = 'clear';
+            this.size = 5;
+            this.point = [0,0];
+            this.lineWidth = 1;
+            this.alpha = 1;
+            
+            /**
+             * Draw an ellipse onto the canvas
+             */
+            this.draw = function(context) {
+                try {
+                    context.lightbulb(this.point[0], this.point[1], this.size, this.colour, this.border, this.lineWidth, this.alpha, this.base);
+                    return true;
+                } catch(err) {
+                    console.log("ERROR - LIGHTBULB: draw() hit an error");
                     console.log(err.name);
                     console.log(err.message);
                 }
@@ -345,14 +551,16 @@ var Shapies = (function(window) {
             /**
              * Some variables
              */
-            this.colour = 'clear';
-            this.eyecolour = 'clear';
-            this.eyeborder = 'clear';
-            this.smilecolour = 'clear';
-            this.smileborder = 'clear';
-            this.border = 'clear';
+            this.colour = 'yellow';
+            this.eyecolour = 'blue';
+            this.eyeborder = 'black';
+            this.smilecolour = 'red';
+            this.smileborder = 'black';
+            this.border = 'black';
+            this.lineWidth = 1;
             this.radius = 100;
             this.point = [0,0];
+            this.alpha = 1;
             
             /**
              * Actions
@@ -375,69 +583,18 @@ var Shapies = (function(window) {
                     /**
                      * Draw the face
                      */
-                    context.beginPath();
-                    context.arc(this.point[0], this.point[1], this.radius, 0, 2*Math.PI, false);
-                    // Colour it in
-                    if (this.colour !== 'clear') {
-                        context.fillStyle = this.colour;
-                        context.fill();
-                    }
-                    // Add in optional border
-                    if (this.border !== 'clear') {
-                        context.lineWidth = 1;
-                        context.strokeStyle = this.border;
-                        context.stroke();
-                    }
-                    // Finish the drawing
-                    context.closePath();
+                    context.circularArc(this.point[0], this.point[1], this.radius, 0, 2*Math.PI, this.colour, this.border, this.lineWidth, false, false, this.alpha);
                     /**
                      * Draw the eyes
                      */
                     for (var i = 0; i < 2; i++) {
                         var base = (i !== 1) ? this.point[0] + (this.radius/ 2) : this.point[0] - (this.radius/ 2);
-                        context.beginPath();
-                        var xLeft =  base - (this.radius/5);
-                        var xRight = base + (this.radius/5);
-                        var yTop = this.point[1] - (this.radius/2) - (this.radius/4);
-                        var yBot = this.point[1] - (this.radius/2) + (this.radius/4);
-                        // NOTE: The moveTo() is needed to start the drawing from the correct spot
-                        context.moveTo(base, yTop);
-                        // Start drawing two bezier curves to create an ellipse
-                        context.bezierCurveTo(xRight, yTop, xRight, yBot, base, yBot);
-                        context.bezierCurveTo(xLeft, yBot, xLeft, yTop, base, yTop);
-                        // Colour it in
-                        if (this.eyecolour !== 'clear') {
-                            context.fillStyle = this.eyecolour;
-                            context.fill();
-                        }
-                        // Add in optional border
-                        if (this.eyeborder !== 'clear') {
-                            context.lineWidth = 1;
-                            context.strokeStyle = this.eyeborder;
-                            context.stroke();
-                        }
-                        // Finish the drawing
-                        context.closePath();
-                        /**
-                         * Draw the smile
-                         */
-                        context.beginPath();
-                        context.arc(this.point[0], this.point[1], this.radius/2, 0, Math.PI, false);
-                        context.closePath();
-                        // Colour it in
-                        if (this.smilecolour !== 'clear') {
-                            context.fillStyle = this.smilecolour;
-                            context.fill();
-                        }
-                        // Add in optional border
-                        if (this.smileborder !== 'clear') {
-                            context.lineWidth = 1;
-                            context.strokeStyle = this.smileborder;
-                            context.stroke();
-                        }
-                        // Finish the drawing
-                        context.closePath();
+                        context.ellipse(base, this.point[1] - (this.radius/2), (this.radius/5), (this.radius/4), this.eyecolour, this.eyeborder, this.lineWidth, this.alpha);
                     }
+                    /**
+                     * Draw the smile
+                     */
+                    context.circularArc(this.point[0], this.point[1], this.radius/2, 0, Math.PI, this.smilecolour, this.smileborder, this.lineWidth, true, false, this.alpha);
                     return true;
                 } catch(err) {
                     console.log("ERROR - SMILIE: draw() hit an error");
@@ -453,6 +610,10 @@ var Shapies = (function(window) {
                 switch (type) {
                     case 'text':
                         return new Text();
+                    case 'roundedrectangle':
+                        return new RoundedRectangle();
+                    case 'lightbulb':
+                        return new Lightbulb();
                     case 'arc':
                         return new Arc();
                     case 'ellipse':
